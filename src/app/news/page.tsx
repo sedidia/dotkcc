0f524054e82b67d2c46498d5379f8fdb4abb033c
sedidia <sedidia01@gmail.com>
2025-10-10 08:11:17 -0700

    other changes

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,19 +1,19 @@
 // app/users/page.tsx
-import { PrismaClient } from '../../generated/prisma/client';
+// import { PrismaClient } from '../../generated/prisma/client';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export default async function UsersPage() {
-  const users = await prisma.user.findMany();
+  // const users = await prisma.user.findMany();
 
   return (
     <div>
       <h1>Liste des utilisateurs</h1>
-      <ul>
+      {/* <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name} ({user.email})</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
